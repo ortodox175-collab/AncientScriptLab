e1e4fe032ca749e23b0147130e41a04ae0c3dd31
ortodox175-collab <ortodox175@gmail.com>
2026-06-27 11:39:04 +0300

    Update and rename record.py to 1

--- record.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from typing import List, Dict, Any
-
-@dataclass
-class TextRecord:
-    script: str
-    text_id: str
-    sequence: List[int]
-    metadata: Dict[str, Any]
